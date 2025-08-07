5fcaff77a26294e61ffb39928078f6e199f18de0
jacquelyn-bradshaw <jacquelyn.bradshaw@flutteruki.com>
2025-08-07 16:46:19 +0100

    Initial styling

--- src/components/task/task.tsx
+++ src/components/task/task.tsx
@@ -1,12 +1,13 @@
 import React from "react";
+import "./task.css";
 
 interface TaskProps {
   task: {
     id: string;
     title: string;
     description?: string;
-    isCompleted?: boolean;
-    dueDate?: Date | undefined;
+    isCompleted: boolean;
+    dueDate?: Date;
   };
 }
 
@@ -14,11 +15,19 @@ const Task: React.FC<TaskProps> = ({ task }) => {
   return (
     <li>
       <h3>{task.title}</h3>
-      <p>{task.description}</p>
-      {task.isCompleted && <p>Completed</p>}
-      {task.dueDate && (
-        <p>Due: {new Date(task.dueDate).toLocaleDateString()}</p>
-      )}
+      <div className="task-details">
+        <div className="task-description">
+          <p>{task.description}</p>
+          {task.isCompleted && (
+            <span className="completed-icon" aria-label="Completed" role="img">
+              ✅
+            </span>
+          )}
+        </div>
+        {task.dueDate && (
+          <p>Due: {new Date(task.dueDate).toLocaleDateString()}</p>
+        )}
+      </div>
     </li>
   );
 };
